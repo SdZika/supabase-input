1cf4d812abb5ba99a314dce2a46b0dc55a71d196
SdZika <markozivkovic1982@gmail.com>
2024-08-15 08:38:37 +0200

    solved how to prevent crash if supabase not working

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,15 +4,37 @@ import { supabase } from "./config/supabaseClient";
 export const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [errorMessage, setError] = useState<string | null>(null)
+  const [successMessage, setSuccess] = useState<string | null>(null)
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = await supabase.from("test").insert([{ name, email }]);
+
+    if (!supabase) {
+      setError("Service is currently unavailable. Please try again later.");
+      return;
+    }
+
+    try {
+
+    } catch(err) {
+      console.error("unespected error", err)
+      setError("Unespected error, try again later")
+    }
+
+    const {data, error} = await supabase.from("test").insert([{ name, email }]);
     console.log("Data: ", data);
+    if(error) {
+      console.error(error)
+      setError("Please try again, there was an issue with sending data to the server")
+    } else {
+      setSuccess("You sent data to the server")
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
+        <form onSubmit={handleSubmit}>
       <input
         type="text"
         name="name"
@@ -29,5 +51,9 @@ export const Contact = () => {
       ></input>
       <button type="submit">Submit</button>
     </form>
+    {errorMessage && <p>{errorMessage}</p>}
+    {successMessage && <p>{successMessage}</p>}
+    </>
+
   );
 };
--- src/components/config/supabaseClient.tsx
+++ src/components/config/supabaseClient.tsx
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
 const supabaseKey = process.env.REACT_APP_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+let supabase: any | null = null;
+
+if (supabaseUrl && supabaseKey) {
+    supabase = createClient(supabaseUrl, supabaseKey);
+  } else {
+    console.error("Supabase URL and Key must be provided.");
+  }
+
+export {supabase};
